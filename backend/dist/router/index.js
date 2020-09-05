88d703482c9918068bd1c9098dbb2928e00f6c6a
madhur4444 <mksmks44444444@gmail.com>
2020-09-05 12:56:16 +0530

    cleanup git

--- backend/dist/router/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function router(app) {
-    app.route("/").get((req, res) => {
-        res.send("Started");
-    });
-}
-exports.default = router;
-//# sourceMappingURL=index.js.map
